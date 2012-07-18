3f168b9bd0c3a1680d63edd7976f0e9a0c1224e1
Drew Ditthardt <dditthardt@olivinelabs.com>
2012-07-17 18:49:28 -0700

    stubbed out folder structure. stubbed out lusty.lua

--- src/lusty.lua
+++ src/lusty.lua
@@ -1,9 +1,23 @@
 package.path = './?.lua;../lib/?.lua;'..package.path
 
-local Object = require('object')
-
 local Lusty = {
-
+  Object = require('object'),
+  event = require('mediator.mediator')(),
+  server = require('server.base'), --base server stub, overridden by config
+  request = function(path, headers, body)
+    --TODO:: creates and processes a new request context
+  end
 }
 
-return Lusty
+return setmetatable(Lusty,
+{
+  __call = function()
+    --TODO:: includes config files which set up event handlers
+    --and fires initial request event
+    self.request(
+      self.server.request.path,
+      self.server.request.headers,
+      self.server.request.body
+    )
+  end
+})
