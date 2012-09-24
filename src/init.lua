69b7f72531ec13b5810ba0b21d76be4a1d6dcf97
Drew Ditthardt <dditthardt@olivinelabs.com>
2012-09-24 12:37:18 -0700

    woops, missed a file on last commit

--- src/init.lua
+++ src/init.lua
@@ -1,28 +1,9 @@
 return setmetatable({
   --Lusty
   event       = require 'mediator'(),
-  publishers  = {},
-  subscribers = {},
-  options     = require 'say',
-  server      = 'base', --base server stub, overridden by config
-  path        = 'config',
+  config      = require 'config',
+  server      = {},
   loaded      = {},
-
-  log         = require 'log',
-
-  --Run config file with lusty as context
-  configure = function(self, file)
-    if type(self) == "string" then
-      file = self
-      self = nil
-    end
-    if not self then self = getfenv(2) end
-    local f = package.loaders[2](self.path..'/'..file)
-    if type(f) == "string" then error(f, 2) end
-    self.options:set_namespace(file)
-    setfenv(f, self)()
-    self.options:set_namespace('lusty')
-  end,
 
   --Lazily load subscribers on publish
   publish = function(self, channel, context)
@@ -36,7 +17,7 @@ return setmetatable({
       end
     end
 
-    local list = self.subscribers
+    local list = self.config.subscribers
     local currentNamespace = {}
     --used to store a record of loaded namespaces
     local loaded = self.loaded
@@ -54,36 +35,50 @@ return setmetatable({
   end,
 
   --Publish events
-  process = function(self, context)
-    for _,channel in pairs(self.publishers) do
+  processPublishers = function(self, context)
+    for _,channel in pairs(self.config.publishers) do
       self:publish(channel, context)
+    end
+  end,
+  --Add interfaces to context
+  processInterfaces = function(self, context)
+    for _,v in pairs(self.config.interfaces) do
+      package.loaders[2]('interface.'..v)(self, context)
     end
   end
 },
 {
   --Lusty Meta-Table
   __call = function(self, path)
-    self.path = path or self.path
+    self.config.path = path or self.config.path
 
-    self.options:set_fallback('lusty')
-    self.options:set_namespace('lusty')
+    self.config.options:set_fallback('lusty')
+    self.config.options:set_namespace('lusty')
 
-    self:configure('lusty')
-    self:configure('subscribers')
-    self:configure('publishers')
+    self.config('lusty')
+    self.config('subscribers')
+    self.config('publishers')
+    self.config('interfaces')
 
-    self.server = require('server.'..self.server)
+    self.server = require('server.'..self.config.server)
 
     local context = {
-      lusty     = self,
       request   = self.server.request,
       response  = self.server.response,
-      data      = {}
+      --data work table.
+      --Used to manipulate response data before output
+      data      = {}, 
     }
-    self:process(context)
+
+    --load interfaces, they set themselves up on context
+    self:processInterfaces(context)
+
+    --Do events, publish with context
+    self:processPublishers(context)
+
     --sets say back to defaults, for other libraries that might use it.
-    self.options:set_fallback('en')
-    self.options:set_namespace('en')
+    self.config.options:set_fallback('en')
+    self.config.options:set_namespace('en')
 
     return context
   end
