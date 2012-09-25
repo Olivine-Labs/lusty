6a2981d16a099c7a88393069bed6b28ce1b34226
Drew Ditthardt <dditthardt@olivinelabs.com>
2012-09-24 23:20:05 -0700

    fixed bug where the config function was not exposed to config files.

--- src/config.lua
+++ src/config.lua
@@ -1,3 +1,16 @@
+local config = function(self, file)
+  if type(self) == "string" then
+    file = self
+    self = nil
+  end
+  if not self then self = getfenv(2) end
+  local f = package.loaders[2](self.path..'/'..file)
+  if type(f) == "string" then error(f, 2) end
+  self.options:set_namespace(file)
+  setfenv(f, self)()
+  self.options:set_namespace('lusty')
+end
+
 return setmetatable({
   options     = require 'say',
   publishers  = {},
@@ -5,19 +18,9 @@ return setmetatable({
   interfaces  = {},
   server      = 'base',
   path        = 'config',
+  config      = config
 },
 {
   --Run config file with self as context
-  __call = function(self, file)
-    if type(self) == "string" then
-      file = self
-      self = nil
-    end
-    if not self then self = getfenv(2) end
-    local f = package.loaders[2](self.path..'/'..file)
-    if type(f) == "string" then error(f, 2) end
-    self.options:set_namespace(file)
-    setfenv(f, self)()
-    self.options:set_namespace('lusty')
-  end
+  __call = config
 })
