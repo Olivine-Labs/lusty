7770371ef9aee392dca1737400baf5424998e48d
Drew Ditthardt <dditthardt@olivinelabs.com>
2012-10-02 12:16:43 -0700

    updated store.lua, changed interface

--- src/interface/store.lua
+++ src/interface/store.lua
@@ -1,8 +1,20 @@
 local lusty, context = ...
 lusty.config('store')
-context.store = function(object, method)
-  lusty:publish({'store'}, {
-    context = context,
-    object = object
-  })
+
+local store = function(method)
+  return function(query)
+    lusty:publish({'store', method}, query)
+  end
 end
+
+context.store = setmetatable({},
+{
+  __index = function(self, key)
+    local val = rawget(self, key)
+    if not val then
+      self[key] = store(key)
+    else
+      return self[key]
+    end
+  end
+})
